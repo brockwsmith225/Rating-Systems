9c5635381471970bbe43eaae27536a3e3f0af2d2
Brock Smith <brocksmith225@gatech.edu>
2019-04-21 14:37:34 -0400

    Added a constructor to create a Rating System with data from a file specified from the user and an interpreter to read said data

--- src/ratingSystem/RatingSystem.java
+++ src/ratingSystem/RatingSystem.java
@@ -3,6 +3,7 @@ package ratingSystem;
 import interpreter.Interpreter;
 import interpreter.datatypes.Entity;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -13,11 +14,25 @@ public abstract class RatingSystem {
     private Interpreter interpreter;
 
     /**
-     * Creates a new instance of a Rating System object with no data
+     * Creates a new instance of a Rating System with no data
      */
     public RatingSystem() {
         entities = new HashMap<>();
         rankedEntities = new ArrayList<>();
         rankedGroups = new ArrayList<>();
+    }
+
+    /**
+     * Creates a new instance of a Rating System with the data found in the file specified by the file path
+     *
+     * @param interpreter the interpreter to be used to read in the data from the data file
+     * @param filePath the file path of the data file
+     * @throws FileNotFoundException if the file specified by the file path is not found
+     */
+    public RatingSystem(Interpreter interpreter, String filePath) throws FileNotFoundException {
+        entities = interpreter.parseData(filePath);
+        rankedEntities = new ArrayList<>();
+        rankedGroups = new ArrayList<>();
+        this.interpreter = interpreter;
     }
 }
