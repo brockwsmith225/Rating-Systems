7cf71c960fc75e06c0fe1a873963334c25e6be23
Brock Smith <brocksmith225@gatech.edu>
2019-04-21 14:27:12 -0400

    Added necessary attributes for a Rating System object

--- src/ratingSystem/RatingSystem.java
+++ src/ratingSystem/RatingSystem.java
@@ -1,5 +1,14 @@
 package ratingSystem;
 
-public abstract class RatingSystem {
+import interpreter.Interpreter;
+import interpreter.datatypes.Entity;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public abstract class RatingSystem {
+    private HashMap<String, Entity> entities;
+    private ArrayList<Entity> rankedEntities;
+    private ArrayList<Entity> rankedGroups;
+    private Interpreter interpreter;
 }
