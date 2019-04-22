aa19cd8973d9dea6e45103fb17c69f7bd65ca9d4
Brock Smith <brocksmith225@gatech.edu>
2019-04-22 16:24:09 -0400

    Added method to setup the 2D array of positive values

--- src/rrs/RRS.java
+++ src/rrs/RRS.java
@@ -1,8 +1,15 @@
 package rrs;
 
+import interpreter.datatypes.DataPoint;
 import ratingSystem.RatingSystem;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class RRS extends RatingSystem {
+
+    private HashMap<String, Integer> entityNameToIndex;
+    private HashMap<Integer, String> entityIndexToName;
 
     @Override
     public void setup() {
@@ -15,7 +22,22 @@ public class RRS extends RatingSystem {
     @Override
     public void rankGroups() {}
 
+
+
     //========== RRS only methods ==========
+    private double[][] setupPositiveValues(int numberOfEntities) {
+        double[][] positiveValues = new double[numberOfEntities][numberOfEntities];
+        for (String entity : entities.keySet()) {
+            ArrayList<DataPoint> dataPoints = entities.get(entity).getDataPoints();
+            for (DataPoint dataPoint : dataPoints) {
+                if (dataPoint.getWeightedScoreDiff() > 0) {
+                    positiveValues[entityNameToIndex.get(entity)][entityNameToIndex.get(dataPoint.getOtherEntity())] = Math.abs(dataPoint.getWeightedScoreDiff());
+                }
+            }
+        }
+        return positiveValues;
+    }
+
     private double[][] setupPartials(int numberOfEntities) {
         double[][] partials = new double[numberOfEntities][numberOfEntities];
         for (int r = 0; r < numberOfEntities; r++) {
