9e69c9bdd4271802d7217b1458005ed1eb7102b0
Brock Smith <brocksmith225@gatech.edu>
2019-04-22 16:36:47 -0400

    Added Matrix and Vector classes which will be needed by RRS

---
+++ src/rrs/datatypes/Matrix.java
@@ -0,0 +1,181 @@
+package rrs.datatypes;
+
+import java.util.Collection;
+
+public class Matrix {
+    protected double[][] matrix;
+
+    /**
+     * Constructs a new matrix with {@code rows} rows and {@code columns} columns.
+     *
+     * @param rows Number of rows in the new matrix
+     * @param columns Number of columns in the new matrix
+     */
+    public Matrix(int rows, int columns) {
+        matrix = new double[rows][columns];
+    }
+
+    /**
+     * Constructs a new matrix from a 2D array.
+     *
+     * @param values 2D array of values to use for the new matrix
+     */
+    public Matrix(double[][] values) {
+        this(values.length, values[0].length);
+        int r = 0;
+        int c = 0;
+        for (double[] vals : values) {
+            for (double entry : vals) {
+                matrix[r][c] = entry;
+                c = (c + 1) % values[0].length;
+            }
+            r++;
+        }
+    }
+
+    /**
+     * Constructs a new matrix from a 2D collection.
+     *
+     * @param values 2D collection of values to use for the new matrix
+     */
+    public Matrix(Collection<Collection<Double>> values) {
+        this(values.size(), values.iterator().next().size());
+        int r = 0;
+        int c = 0;
+        for (Collection<Double> vals : values) {
+            for (double entry : vals) {
+                matrix[r][c++] = entry;
+            }
+            r++;
+        }
+    }
+
+    /**
+     * Returns the number of rows in the matrix.
+     *
+     * @return Number of rows in the matrix
+     */
+    public int rows() {
+        return matrix.length;
+    }
+
+    /**
+     * Returns the number of columns in the matrix.
+     *
+     * @return Number of columns in the matrix
+     */
+    public int columns() {
+        return matrix[0].length;
+    }
+
+    /**
+     * Calculates the sum of two matrices.
+     *
+     * @param m Matrix to be added
+     * @return Resultant matrix
+     */
+    public Matrix add(Matrix m) {
+        if (matrix.length != m.matrix.length || matrix[0].length != m.matrix[0].length) {
+            throw new IllegalArgumentException("Cannot add matrices of different sizes.");
+        }
+
+        double[][] temp = new double[matrix.length][matrix[0].length];
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[r].length; c++) {
+                temp[r][c] = matrix[r][c] + m.matrix[r][c];
+            }
+        }
+        return new Matrix(temp);
+    }
+
+    public Matrix multiply(double s) {
+        double[][] res = new double[matrix.length][matrix[0].length];
+        for (int r = 0; r < res.length; r++) {
+            for (int c = 0; c < res[0].length; c++) {
+                res[r][c] = s * matrix[r][c];
+            }
+        }
+        return new Matrix(res);
+    }
+
+    /**
+     * Calculates the product of a matrix and a vector.
+     *
+     * @param v Vector to be multiplied
+     * @return Resultant vector
+     */
+    public Vector multiply(Vector v) {
+        if (matrix[0].length != v.vector.length) {
+            throw new IllegalArgumentException("Number of columns in the matrix and the vector size must match.");
+        }
+
+        double[] res = new double[matrix.length];
+        for (int r = 0; r < matrix.length; r++) {
+            res[r] = 0;
+            for (int c = 0; c < matrix[r].length; c++) {
+                res[r] += matrix[r][c] * v.vector[c];
+            }
+        }
+        return new Vector(res);
+    }
+
+    /**
+     * Calculates the product of two matrices.
+     *
+     * @param m Matrix to be multiplied
+     * @return Resultant matrix
+     */
+    public Matrix multiply(Matrix m) {
+        if (matrix[0].length != m.matrix.length) {
+            throw new IllegalArgumentException("Number of columns in the first matrix and the number of rows in the second matrix must match.");
+        }
+
+        double[][] res = new double[matrix.length][m.matrix[0].length];
+        for (int r = 0; r < res.length; r++) {
+            for (int c = 0; c < res[r].length; c++) {
+                res[r][c] = 0;
+                for (int i = 0; i < matrix[0].length; i++) {
+                    res[r][c] += matrix[r][i] * m.matrix[i][c];
+                }
+            }
+        }
+        return new Matrix(res);
+    }
+
+    /**
+     * Calculates the row reduced version of the matrix.
+     *
+     * @return Row reduced version of the matrix
+     */
+    public Matrix rowReduce() {
+        Matrix m = new Matrix(matrix);
+        for (int p = 0; p < m.matrix.length; p++) {
+            if (m.matrix[p][p] != 0) {
+                for (int r = 0; r < m.matrix.length; r++) {
+                    double multiplier = m.matrix[r][p] / m.matrix[p][p];
+                    if (r != p) {
+                        for (int c = p; c < m.matrix.length; c++) {
+                            m.matrix[r][c] -= m.matrix[p][c] * multiplier;
+                            if (Math.abs(m.matrix[r][c]) < 0.00000000001) {
+                                m.matrix[r][c] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return m;
+    }
+
+    @Override
+    public String toString() {
+        String res = "";
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[r].length; c++) {
+                res += matrix[r][c] + " ";
+            }
+            res += "\n";
+        }
+        return res;
+    }
+}
---
+++ src/rrs/datatypes/Vector.java
@@ -0,0 +1,137 @@
+package rrs.datatypes;
+
+import java.util.Collection;
+
+public class Vector {
+    protected double[] vector;
+
+    /**
+     * Constructs a new vector of size {@code size}.
+     *
+     * @param size Size of the new vector
+     */
+    public Vector(int size) {
+        vector = new double[size];
+    }
+
+    /**
+     * Constructs a new vector from an array.
+     *
+     * @param values Array of values to use for the new vector
+     */
+    public Vector(double[] values) {
+        this(values.length);
+        int i = 0;
+        for (Double entry : values) {
+            vector[i++] = entry;
+        }
+    }
+
+    /**
+     * Constructs a new vector from a collection.
+     *
+     * @param values Collection of values to use for the new vector
+     */
+    public Vector(Collection<Double> values) {
+        this(values.size());
+        int i = 0;
+        for (Double entry : values) {
+            vector[i++] = entry;
+        }
+    }
+
+    /**
+     * Returns the size of the vector.
+     *
+     * @return The size of the vector;
+     */
+    public int size() {
+        return vector.length;
+    }
+
+    /**
+     * Returns the magnitude of the vector.
+     *
+     * @return Double representation of the magnitude.
+     */
+    public double magnitude() {
+        double mag = 0.0;
+        for (double v : vector) {
+            mag += Math.pow(v, 2);
+        }
+        return Math.sqrt(mag);
+    }
+
+    /**
+     * Returns the sum of the vectors {@code this} and {@code v}.
+     *
+     * @param v Vector to be added.
+     * @return Sum of the vectors.
+     */
+    public Vector add(Vector v) {
+        if (vector.length != v.vector.length) {
+            throw new IllegalArgumentException("Cannot compute the sum of vectors of different sizes.");
+        }
+
+        double[] temp = new double[vector.length];
+        for (int i = 0; i < vector.length; i++) {
+            temp[i] = vector[i] + v.vector[i];
+        }
+        return new Vector(temp);
+    }
+
+    /**
+     * Returns the vector multiplied by the scalar quantity {@code scalar}.
+     *
+     * @param scalar Scalar quantity by which to multiply the vector.
+     * @return Vector multiplied by the scalar quantity {@code scalar}.
+     */
+    public Vector multiply(double scalar) {
+        double[] temp = new double[vector.length];
+        for (int i = 0; i < vector.length; i++) {
+            temp[i] = vector[i] * scalar;
+        }
+        return new Vector(temp);
+    }
+
+    /**
+     * Returns the dot product of the vectors {@code this} and {@code v}.
+     *
+     * @param v Vector to be used in the dot product calculation.
+     * @return Double representation of the dot product.
+     */
+    public double dotProduct(Vector v) {
+        if (vector.length != v.vector.length) {
+            throw new IllegalArgumentException("Cannot compute the dot product of vectors of different sizes.");
+        }
+
+        double dotProd = 0.0;
+        for (int i = 0; i < vector.length; i++) {
+            dotProd += vector[i] * v.vector[i];
+        }
+        return dotProd;
+    }
+
+    /**
+     * Returns the cosine similarity of the vectors {@code this} and {@code v}.
+     *
+     * @param v Vector to be used in the cosine similarity calculation.
+     * @return Double representation of the dot product.
+     */
+    public double cosineSimilarity(Vector v) {
+        if (vector.length != v.vector.length) {
+            return 0.0;
+        }
+        return this.dotProduct(v) / (this.magnitude() * v.magnitude());
+    }
+
+    @Override
+    public String toString() {
+        String res = "";
+        for (int i = 0; i < vector.length; i++) {
+            res += vector[i] + ", ";
+        }
+        res = res.substring(0, res.length() - 2);
+        return res;
+    }
+}
