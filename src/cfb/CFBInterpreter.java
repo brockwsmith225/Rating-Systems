534cd9d0903550d7be5fe8770476852bcfa19632
Brock Smith <brocksmith225@gatech.edu>
2019-04-23 12:03:37 -0400

    Added a new Interpreter to interpret College Football data

---
+++ src/cfb/CFBInterpreter.java
@@ -0,0 +1,25 @@
+package cfb;
+
+import interpreter.Interpreter;
+import interpreter.LimitingFunction;
+import interpreter.datatypes.Entity;
+
+import java.util.HashMap;
+
+public class CFBInterpreter extends Interpreter {
+
+    @Override
+    public HashMap<String, Entity> parseData(String filePath) {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Entity> parseData(String filePath, LimitingFunction limitingFunction) {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Entity> parseData(String[] filePaths) {
+        return null;
+    }
+}
