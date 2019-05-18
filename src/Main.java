68666cae4a1bba9f4b583ea3745ac8ac42bbf7ce
Brock Smith <brocksmith225@gatech.edu>
2019-05-18 11:23:32 -0400

    Added test for RRS and CFB

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import ratingSystem.RatingSystem;
+import rrs.RRS;
+
 public class Main {
     public static void main(String[] args) {
-
+        RatingSystem rrs = new RRS();
+        rrs.setup();
+        rrs.printEntityRankings();
     }
 }
