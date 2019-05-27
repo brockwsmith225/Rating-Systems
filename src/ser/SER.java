9f43bdf10cd46686b177eb4b91f759f60bc13f0c
Brock Smith <brocksmith225@gatech.edu>
2019-05-27 16:09:22 -0400

    Added SER package and class for the Simple Efficiency Rating system

---
+++ src/ser/SER.java
@@ -0,0 +1,20 @@
+package ser;
+
+import ratingSystem.RatingSystem;
+
+public class SER extends RatingSystem {
+
+    @Override
+    public void setup() {}
+
+    @Override
+    public void rankEntities() {}
+
+    @Override
+    public void rankGroups() {}
+
+
+
+    //========== SER only methods ==========
+
+}
