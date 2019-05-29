16e7ed9a6ec9b5ea548d8ca1a49902dafeaf178e
Brock Smith <brocksmith225@gatech.edu>
2019-05-28 22:36:21 -0400

    Renamed CFB to College Football to be more clear

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-import cfb.CFBInterpreter;
+import collegeFootball.CollegeFootballInterpreter;
 import ratingSystem.RatingSystem;
 import relativeRatingSystem.RelativeRatingSystem;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            RatingSystem rrs = new RelativeRatingSystem(new CFBInterpreter(), 2018);
+            RatingSystem rrs = new RelativeRatingSystem(new CollegeFootballInterpreter(), 2018);
             rrs.setup();
             rrs.printTeamRankings();
         } catch (Exception e) {
--- src/collegeFootball/CollegeFootballInterpreter.java
+++ src/collegeFootball/CollegeFootballInterpreter.java
@@ -1,4 +1,4 @@
-package cfb;
+package collegeFootball;
 
 import interpreter.Interpreter;
 import interpreter.datatypes.Game;
@@ -12,11 +12,11 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 
-public class CFBInterpreter extends Interpreter<Integer> {
+public class CollegeFootballInterpreter extends Interpreter<Integer> {
 
     @Override
     public HashMap<String, Team> parseData(int year) throws FileNotFoundException {
-        Scanner data = new Scanner(new File("ratingsystems/src/cfb/data/cfb-" + year + ".csv"));
+        Scanner data = new Scanner(new File("ratingsystems/src/collegeFootball/data/collegeFootball-" + year + ".csv"));
         teams = new HashMap<>();
         groups = new ArrayList<>();
         addedTeams = new HashSet<>();
@@ -57,13 +57,13 @@ public class CFBInterpreter extends Interpreter<Integer> {
 
     @Override
     public HashMap<String, Team> parseData(int year, Integer week) throws FileNotFoundException {
-        Scanner data = new Scanner(new File("ratingsystems/src/cfb/data/cfb-" + year + ".csv"));
+        Scanner data = new Scanner(new File("ratingsystems/src/collegeFootball/data/collegeFootball-" + year + ".csv"));
         teams = new HashMap<>();
         addedTeams = new HashSet<>();
 
         Time startDate = getStartDate(data);
 
-        data = new Scanner(new File("ratingsystems/src/cfb/data/cfb-" + year + ".csv"));
+        data = new Scanner(new File("ratingsystems/src/collegeFootball/data/collegeFootball-" + year + ".csv"));
         while (data.hasNext()) {
             String[] line = split(data.nextLine(), ",");
 
@@ -110,11 +110,11 @@ public class CFBInterpreter extends Interpreter<Integer> {
         teams = new HashMap<>();
         addedTeams = new HashSet<>();
         for (int year : years) {
-            Scanner data = new Scanner(new File("ratingsystems/src/cfb/data/cfb-" + year + ".csv"));
+            Scanner data = new Scanner(new File("ratingsystems/src/collegeFootball/data/collegeFootball-" + year + ".csv"));
 
             Time startDate = getStartDate(data);
 
-            data = new Scanner(new File("ratingsystems/src/cfb/data/cfb-" + year + ".csv"));
+            data = new Scanner(new File("ratingsystems/src/collegeFootball/data/collegeFootball-" + year + ".csv"));
             while (data.hasNext()) {
                 String[] line = split(data.nextLine(), ",");
 
@@ -150,7 +150,7 @@ public class CFBInterpreter extends Interpreter<Integer> {
 
     @Override
     public boolean hasData(int year)  {
-        return new File("ratingsystems/src/cfb/data/cfb-" + year + ".csv").exists();
+        return new File("ratingsystems/src/collegeFootball/data/collegeFootball-" + year + ".csv").exists();
     }
 
     //========== CFB only methods ==========
