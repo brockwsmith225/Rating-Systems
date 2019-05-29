4064ac8d3292096125893330cb9facb09257ea59
Brock Smith <brocksmith225@gatech.edu>
2019-05-28 22:38:42 -0400

    Wrote rankTeams method for SER, added rankTeams and rankGroups calls to SER setup method

--- src/simpleEfficiencyRating/SimpleEfficiencyRating.java
+++ src/simpleEfficiencyRating/SimpleEfficiencyRating.java
@@ -2,13 +2,24 @@ package simpleEfficiencyRating;
 
 import ratingSystem.RatingSystem;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class SimpleEfficiencyRating extends RatingSystem {
 
     @Override
-    public void setup() {}
+    public void setup() {
+
+
+        rankTeams();
+        rankGroups();
+    }
 
     @Override
-    public void rankTeams() {}
+    public void rankTeams() {
+        rankedTeams = new ArrayList<>(teams.values());
+        Collections.sort(rankedTeams);
+    }
 
     @Override
     public void rankGroups() {}
