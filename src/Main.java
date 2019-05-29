88835f6505a1e1eac115870d700cffaa016c13f7
Brock Smith <brocksmith225@gatech.edu>
2019-05-28 23:07:46 -0400

    Rewrote main method to test SER ratings

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 import collegeFootball.CollegeFootballInterpreter;
 import ratingSystem.RatingSystem;
-import relativeRatingSystem.RelativeRatingSystem;
 import simpleEfficiencyRating.SimpleEfficiencyRating;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            RatingSystem rrs = new SimpleEfficiencyRating(new CollegeFootballInterpreter(), 2018);
-            rrs.setup();
-            rrs.printTeamRankings();
+            RatingSystem ser = new SimpleEfficiencyRating(new CollegeFootballInterpreter(), 2018);
+            ser.setup();
+            ser.printTeamRankings();
         } catch (Exception e) {
             e.printStackTrace();
         }
