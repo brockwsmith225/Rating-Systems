a28036a5330072ea951366a9cc617662e09e9b7c
Brock Smith <brocksmith225@gatech.edu>
2019-07-17 22:10:16 -0400

    Implemented pretty-print option in predict command

--- src/ratingsystems-common/src/ratingsystems/common/cli/commands/Predict.java
+++ src/ratingsystems-common/src/ratingsystems/common/cli/commands/Predict.java
@@ -2,13 +2,25 @@ package ratingsystems.common.cli.commands;
 
 import ratingsystems.common.cli.CommandInput;
 import ratingsystems.common.cli.Runner;
+import ratingsystems.common.cli.Terminal;
 
 public class Predict extends Command {
     @Override
     public void run(Runner runner, CommandInput commandInput) {
         String team1 = commandInput.getArgs().get(0);
         String team2 = commandInput.getArgs().get(1);
-        System.out.println(runner.loadRatingSystem(commandInput).predictGame(team1, team2));
+        double odds = runner.loadRatingSystem(commandInput).predictGame(team1, team2);
+
+        if (commandInput.getOption("pretty-print")) {
+            System.out.println();
+            System.out.println(team1 + " vs " + team2);
+            System.out.println("----------");
+            System.out.println(team1 + ": " + Terminal.round(odds * 100, 2) + "%");
+            System.out.println(team2 + ": " + Terminal.round((1 - odds) * 100, 2) + "%");
+            System.out.println();
+        } else {
+            System.out.println(odds);
+        }
     }
 
     @Override
