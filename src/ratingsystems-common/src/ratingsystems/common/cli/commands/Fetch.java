46575630f6af2d78489a660582eaae081580668a
Brock Smith <brocksmith225@gatech.edu>
2019-09-11 14:13:23 -0400

    Added fetch command to scrape data from Sports Reference

---
+++ src/ratingsystems-common/src/ratingsystems/common/cli/commands/Fetch.java
@@ -0,0 +1,32 @@
+package ratingsystems.common.cli.commands;
+
+import ratingsystems.common.cli.CommandInput;
+import ratingsystems.common.cli.Runner;
+
+public class Fetch extends Command {
+    @Override
+    public void run(Runner runner, CommandInput commandInput) {
+        try {
+            runner.getInterpreter(runner.getParameterValue("LEAGUE").toString()).fetchData((int) runner.getParameterValue("YEAR"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean validateInput(Runner runner, CommandInput commandInput) {
+        try {
+            Process process = Runtime.getRuntime().exec("ping www.google.com");
+            int res = process.waitFor();
+            if (res != 0) {
+                System.err.println("ERROR: Not connected to the internet");
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+}
