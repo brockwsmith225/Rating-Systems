fc0bb59559790e2d900fe324c15b135ee75419a7
Brock Smith <brocksmith225@gatech.edu>
2019-09-18 19:08:54 -0400

    Added set of allowed commands to the API

--- api/src/main/java/ratingsystems/api/Controller.java
+++ api/src/main/java/ratingsystems/api/Controller.java
@@ -8,17 +8,23 @@ import ratingsystems.hps.HistoricalPredictionSystemRunner;
 import ratingsystems.rrs.RelativeRatingSystemRunner;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
 @RestController
 public class Controller {
     private HashMap<String, Runner> ratingSystems;
+    private HashSet<String> allowedCommands;
 
     public Controller() {
         ratingSystems = new HashMap<>();
         ratingSystems.put("rrs", new RelativeRatingSystemRunner());
         ratingSystems.put("hps", new HistoricalPredictionSystemRunner());
+
+        allowedCommands = new HashSet<>();
+        allowedCommands.add("rank");
+        allowedCommands.add("predict");
     }
 
     @RequestMapping("/api/{system}/{command}")
@@ -26,6 +32,9 @@ public class Controller {
         system = system.toLowerCase();
         if (!ratingSystems.containsKey(system)) {
             return new Error("Rating system " + system + " does not exist");
+        }
+        if (!allowedCommands.contains(command)) {
+            return new Error("Command " + command + " does not exist");
         }
 
         HashMap<String, Boolean> options = null;
