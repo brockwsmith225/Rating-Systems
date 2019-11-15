99aa46af2caf42ceebc4f6aaa76c571daef2c9bf
Brock Smith <brocksmith225@gatech.edu>
2019-11-14 21:06:01 -0500

    Updated HPS with more statistic and similarity options to play around with

--- hps/src/main/java/ratingsystems/hps/HPSTeam.java
+++ hps/src/main/java/ratingsystems/hps/HPSTeam.java
@@ -10,8 +10,8 @@ import java.util.Map;
 
 public class HPSTeam extends SERTeam {
 
-    Map<String, Double> avgStats;
-    Map<String, Double> scaledStats;
+    private Map<String, Double> avgStats;
+    private Map<String, Double> scaledStats;
 
     public HPSTeam(Team team) {
         super(team);
@@ -32,26 +32,70 @@ public class HPSTeam extends SERTeam {
         scaledStats = new HashMap<>();
         for (Game game : team.getGames()) {
             for (String stat : game.getStatistics().keySet()) {
+                double scaledStat = Math.exp(game.getStatistic(stat) / teams.get(game.getOpponent()).avgStats.get(stat) - 1);
+                if (Double.isNaN(scaledStat) || Double.isInfinite(scaledStat)) scaledStat = 1.0;
                 if (scaledStats.containsKey(stat)) {
-                    scaledStats.put(stat, scaledStats.get(stat) + game.getStatistic(stat) / teams.get(game.getOpponent()).avgStats.get(stat));
+                    scaledStats.put(stat, scaledStats.get(stat) * scaledStat);
                 } else {
-                    scaledStats.put(stat, game.getStatistic(stat) / teams.get(game.getOpponent()).avgStats.get(stat));
+                    scaledStats.put(stat, scaledStat);
                 }
             }
         }
-        scaledStats.replaceAll((key, value) -> {
-            double newValue = value / team.getNumberOfGames();
-            if (Double.isNaN(newValue)) {
-                return 1.0;
-            }
-            if (Double.isInfinite(newValue) || newValue > 10) {
-                return 10.0;
-            }
-            if (newValue < 0.1) {
-                return 0.1;
-            }
-            return newValue;
-        });
+        scaledStats.replaceAll((key, value) -> Math.pow(value, 1.0 / team.getNumberOfGames()));
+    }
+
+    public double offensePassingPlayPercent() {
+        return avgStats.get("PassAttempts") / (avgStats.get("PassAttempts") + avgStats.get("RushAttempts"));
+    }
+
+    public double offenseRushingPlayPercent() {
+        return avgStats.get("RushAttempts") / (avgStats.get("PassAttempts") + avgStats.get("RushAttempts"));
+    }
+
+    public double offensePassingEfficiency() {
+        double scoreEff = scaledStats.get("PassTDs");
+        double yardEff = scaledStats.get("PassYards");
+        double compEff = scaledStats.get("PassCompletions") / scaledStats.get("PassAttempts");
+        return Math.pow(scoreEff * yardEff * compEff, 1.0 / 3.0);
+    }
+
+    public double offenseRushingEfficiency() {
+        double scoreEff = scaledStats.get("RushTDs");
+        double yardEff = scaledStats.get("RushYards");
+        return Math.pow(scoreEff * yardEff, 1.0 / 2.0);
+    }
+
+    public double offenseEfficiency() {
+        double offPassEff = offensePassingEfficiency();
+        double offRushEff = offenseRushingEfficiency();
+        return Math.pow(offPassEff, offensePassingPlayPercent()) * Math.pow(offRushEff, offenseRushingPlayPercent());
+    }
+
+    public double defensePassingPlayPercent() {
+        return avgStats.get("OpponentPassAttempts") / (avgStats.get("OpponentPassAttempts") + avgStats.get("OpponentRushAttempts"));
+    }
+
+    public double defenseRushingPlayPercent() {
+        return avgStats.get("OpponentRushAttempts") / (avgStats.get("OpponentPassAttempts") + avgStats.get("OpponentRushAttempts"));
+    }
+
+    public double defensePassingEfficiency() {
+        double scoreEff = scaledStats.get("OpponentPassTDs");
+        double yardEff = scaledStats.get("OpponentPassYards");
+        double compEff = scaledStats.get("OpponentPassCompletions") / scaledStats.get("OpponentPassAttempts");
+        return 1.0 / Math.pow(scoreEff * yardEff * compEff, 1.0 / 3.0);
+    }
+
+    public double defenseRushingEfficiency() {
+        double scoreEff = scaledStats.get("OpponentRushTDs");
+        double yardEff = scaledStats.get("OpponentRushYards");
+        return 1.0 / Math.pow(scoreEff * yardEff, 1.0 / 2.0);
+    }
+
+    public double defenseEfficiency() {
+        double defPassEff = defensePassingEfficiency();
+        double defRushEff = defenseRushingEfficiency();
+        return Math.pow(defPassEff, defensePassingPlayPercent()) * Math.pow(defRushEff, defenseRushingPlayPercent());
     }
 
     public double similarity(HPSTeam team) {
@@ -62,14 +106,26 @@ public class HPSTeam extends SERTeam {
 //            return 0.0;
 //        } else if (Double.isInfinite(similarity)) {
 //            return 1.0;
-//        }3
+//        }
 //        return similarity;
-        double similarity = 0.0;
-        for (String stat : scaledStats.keySet()) {
-            double statSim = scaledStats.get(stat) / team.scaledStats.get(stat);
-            similarity += statSim <= 1 ? statSim : 1.0 / statSim;
-        }
-        return similarity / scaledStats.size();
+//        double similarity = 1.0;
+//        for (String stat : scaledStats.keySet()) {
+//            double statSim = scaledStats.get(stat) / team.scaledStats.get(stat);
+//            similarity *= statSim <= 1 ? statSim : 1.0 / statSim;
+//        }
+//        return Math.pow(similarity, 1.0 / scaledStats.size());
+        double similarity = 1.0;
+        similarity *= multAbs(offensePassingEfficiency() / team.offensePassingEfficiency());
+        similarity *= multAbs(offenseRushingEfficiency() / team.offenseRushingEfficiency());
+        similarity *= multAbs(offensePassingPlayPercent() / team.offensePassingPlayPercent());
+        similarity *= multAbs(defensePassingEfficiency() / team.defensePassingEfficiency());
+        similarity *= multAbs(defenseRushingEfficiency() / team.defenseRushingEfficiency());
+        similarity *= multAbs(defensePassingPlayPercent() / team.defensePassingPlayPercent());
+        return Math.pow(similarity, 1.0 / 6.0);
+    }
+
+    private double multAbs(double x) {
+        return x <= 1 ? x : 1.0 / x;
     }
 
 }
